1b0a6a65fe86542f50afa9c01c2cd734249e7628
Preethigouda <147792710+Preethigouda@users.noreply.github.com>
2026-09-25 15:13:05 +0530

    Create program3.cpp

---
+++ Programs/Class2/program3.cpp
@@ -0,0 +1,35 @@
+// write a c++ program to create a Student Class with data members name, age and display the details using an object.
+
+// Previous code(prog_2) with {{Writing the class functions outside of class}}...
+
+
+#include<iostream>
+using namespace std;
+
+ class Student
+ {
+  string s1;
+  int age;
+  public:
+       void DisplayData(); 
+
+      void SetData()
+      {
+        s1="Kingson";
+          age=21;
+      }
+ };
+
+ void Student::DisplayData()
+      {
+          cout<<"Name is : "<<s1<<endl;
+          cout<<"Age is : "<<age;
+      }
+
+ int main()
+ {
+     Student s1;
+     s1.SetData();
+     s1.DisplayData();
+     return 0;
+ }
